fe6dafbc01b012b4c0e1325a9659e2dcdbc57a44
Евгений Пустовалов <techsmith314@hotmail.com>
2023-01-24 20:50:03 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int imagesInRow = 3;
+            int allUserImages = 52;
+            int numberOfRows = allUserImages / imagesInRow;
+            int remainsImages = allUserImages - (numberOfRows * imagesInRow);
+
+            Console.WriteLine($"Полностью заполненых рядов: {numberOfRows}\nКоличество картинок неполного ряда: {remainsImages}");
+        }
+    }
+}
